caac77664d3b8745e5d1c71b6b87c79d07b65a92
Edimah <98950952+Edimah@users.noreply.github.com>
2025-09-15 12:16:44 -0400

    added all node types exploration for healh data xml file

--- R/health_data_import.R
+++ R/health_data_import.R
@@ -1,6 +1,4 @@
 
-rm(list = ls())
-
 library(xml2)
 library(dplyr)
 library(lubridate)
@@ -10,6 +8,16 @@ library(lubridate)
 #######################
 
 health_data <- read_xml("personal-projects/data/health_data_export.xml")
+
+# Extract all node types
+all_nodes <- xml_find_all(health_data, "//*")
+n_nodes <- length(all_nodes)
+print(paste("Number of different node types:", n_nodes))
+node_types <- unique(xml_name(all_nodes))
+print(paste("Total unique node types:", length(node_types)))
+print("All unique node types:")
+print(node_types, 20)
+
 
 # Extract all <Record> nodes
 records <- xml_find_all(health_data, "//Record")
@@ -22,9 +30,9 @@ print("First 20 unique record types:")
 print(head(record_types, 20))
 print(paste("Total unique record types:", length(record_types)))
 
-##############################################################
-# Building CSV file out of xml data for heatmap visuaisation #
-##############################################################
+######################################
+# Building CSV files out of xml data #
+######################################
 
 
 path_out <- "personal-projects/data"
@@ -47,17 +55,19 @@ records_df <- tibble(
 full_path_records_csv <- file.path(path_out, "health_data_records.csv")
 write.csv(records_df, full_path_records_csv, row.names = FALSE)
 
-# ---- XML to tibble for metadata ----
-metadata <- xml_find_all(records, ".//MetadataEntry")
 
-metadata_df <- purrr::map_dfr(metadata, function(m) {
-  tibble(
-    record_id   = as.integer(xml_attr(xml_parent(m), "record_id")),
-    parent_type = xml_attr(xml_parent(m), "type"),
-    key         = xml_attr(m, "key"),
-    value       = xml_attr(m, "value")
-  )
-})
+# ------ XML to tibble for workouts -------
 
-full_path_metadata_csv <- file.path(path_out, "health_data_metadata.csv")
-write.csv(records_df, full_path_metadata_csv, row.names = FALSE)
+workout_nodes <- xml_find_all(health_data, "//Workout")
+workouts_df <- tibble::tibble(
+      workout_id      = seq_along(workout_nodes),
+      activity_type   = xml_attr(workout_nodes, "workoutActivityType"),
+      sourceName      = xml_attr(workout_nodes, "sourceName"),
+      sourceVersion   = xml_attr(workout_nodes, "sourceVersion"),
+      device          = xml_attr(workout_nodes, "device"),
+      creationDate_raw= xml_attr(workout_nodes, "creationDate"),
+      startDate_raw   = xml_attr(workout_nodes, "startDate"),
+      endDate_raw     = xml_attr(workout_nodes, "endDate"),
+      duration_s      = as.numeric(xml_attr(workout_nodes, "duration")),
+      totalDistance   = as.numeric(xml_attr(workout_nodes, "totalDistance"))
+    )
